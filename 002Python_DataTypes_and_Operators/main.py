f1e39822d9f200becffd967a67a26de1d7ecf407
luisfsangil <luisfsangil@protonmail.com>
2021-07-29 13:33:18 +0200

    DataTypes_and_Operators

--- 002Python_DataTypes_and_Operators/main.py
+++ 002Python_DataTypes_and_Operators/main.py
@@ -1,4 +1,14 @@
-print("5+6="+5+6)
-print("10%3="+10%3)
-print("5**3="+5**3)
-print("9//2="+9//2)
+#Operators
+print(5+6)
+print(10%3)
+print(5**3)
+print(9//2)
+
+#Data Types
+variable = 5
+print(type(variable))
+variable = "marcus"
+print(type(variable))
+
+#Booleans
+print(5==8)
