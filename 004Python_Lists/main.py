3dd2d623f80c2c78894d3d22b57356a7dea36fb1
luisfsangil <luisfsangil@protonmail.com>
2021-07-29 13:59:17 +0200

    Lists

---
+++ 004Python_Lists/main.py
@@ -0,0 +1,15 @@
+#A list in python its like an array, but the lists can have diferent types inside
+mylist = ["Luis",27,1.85]
+
+mylist.append(78)
+mylist.extend(["blonde","strong","funny",False])
+
+print(mylist[:])
+print(type(mylist))
+print(type(mylist[0]))
+print(type(mylist[1]))
+print(type(mylist[2]))
+print(type(mylist[3]))
+print(type(mylist[4]))
+print(type(mylist[5]))
+print(type(mylist[6]))
