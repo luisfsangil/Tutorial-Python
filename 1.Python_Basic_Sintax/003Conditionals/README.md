0380930cb9ef9b848ff10276d0a3b909eca5fb41
luisfsangil <luisfsangil@protonmail.com>
2021-07-31 12:27:24 +0200

    Conditionals

---
+++ 1.Python_Basic_Sintax/003Conditionals/README.md
@@ -0,0 +1,3 @@
+#Conditionals in python
+
+Some examples to learn how to use the conditionals in python
---
+++ 1.Python_Basic_Sintax/003Conditionals/example1.py
@@ -0,0 +1,9 @@
+#A simple code to verify the age of the user
+
+age=input("Please, enter your age here:")
+age=int(age)
+
+if age<18:
+    print("Sorry, you can't enter here")
+else:
+    print("Welcome")
