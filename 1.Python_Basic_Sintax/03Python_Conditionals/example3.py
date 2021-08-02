29c85c0e68ef8a22afba7929d0512555424c0fa5
luisfsangil <luisfsangil@protonmail.com>
2021-08-02 12:15:12 +0200

    Conditionals

--- 1.Python_Basic_Sintax/03Python_Conditionals/example3.py
+++ 1.Python_Basic_Sintax/03Python_Conditionals/example3.py
@@ -4,10 +4,12 @@ print("In this club only enter girls that have 21 years or more")
 print("If you are a boy or if you don't have 21 yet, you have to pay 50 dolars\n")
 
 sex=bool(input("Are you a girl (say True or False please)? "))
-age=int(input("What's your age? "))
-if not(sex==True and age>=21):
+if sex:
+    age=int(input("What's your age? "))
+if not(sex and age>21):
     payed=bool(input("You Want to pay 50 dolars (say True or False please)? "))
-if (sex==True and age>=21) or payed==True:
+
+if (sex and age>=21) or payed:
     print("Welcome to our club! ")
 else:
     print("Sorry, you can't enter here")
