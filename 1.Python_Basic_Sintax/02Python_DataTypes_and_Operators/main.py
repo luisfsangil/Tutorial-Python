f302e2d6a6f16877f85030914994014db94a1ab1
luisfsangil <luisfsangil@protonmail.com>
2021-08-02 11:36:36 +0200

    Data Types and Operators

--- 1.Python_Basic_Sintax/02Python_DataTypes_and_Operators/main.py
+++ 1.Python_Basic_Sintax/02Python_DataTypes_and_Operators/main.py
@@ -1,14 +1,15 @@
 #Operators
+#In Python exists a lot of diferent operators. Look at somes of them ...
 print(5+6)
 print(10%3)
 print(5**3)
 print(9//2)
 
 #Data Types
-variable = 5
-print(type(variable))
-variable = "marcus"
-print(type(variable))
-
-#Booleans
-print(5==8)
+#In Python we have the predefinied function 'type(<variable>)' to know the data type of the 
+#variable <variable>
+print(type(5))
+print(type("marcus"))
+print(type(3.33))
+print(type(True))
+print(type(1+2j))
