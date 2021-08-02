4be35e1bb5829438fa77d7b594b439e6cc3df04c
luisfsangil <luisfsangil@protonmail.com>
2021-08-02 12:31:25 +0200

    Elif

---
+++ 1.Python_Basic_Sintax/03Python_Conditionals/example4.py
@@ -0,0 +1,8 @@
+#Using 'if', 'elif' and 'else'
+calification = int(input("What's your calification in the exam ?\n"))
+if calification<5:
+    print("Sorry, you have to repeat another year more...\n")
+elif calification<=10:
+    print("You pass the exam\n")
+else:
+    print("Are you kiding me?")
